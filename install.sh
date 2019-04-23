94bae655916c08ec426eac352f846ae9283798bc
Quarks And Leptons <micah112358@gmail.com>
2019-04-23 13:43:20 -0500

    Enable i2c (hopefully)

--- install.sh
+++ install.sh
@@ -3,22 +3,27 @@
 ## go back to the home directory
 cd
 
-echo "Seting up I2C"
-sudo apt-get update
-sudo apt-get install -y i2c-tools libi2c-dev  
-sudo adduser pi i2c
-
 echo "Installing Python 3"
-sudo apt-get install -y python3 python3-pip
+sudo apt-get install -y python3 python3-pip python-dev python3-dev
 
 echo "Installing PiCamera Python libraries"
 sudo apt-get install -y python3-picamera python-picamera
+
+echo "Enabling I2C
+dtparam i2c_arm=on
+modprobe i2c-dev
+
+echo "Seting up I2C"
+sudo apt-get update
+sudo apt-get install -y i2c-tools libi2c-dev  
+sudo apt-get install -y python-smbus python3-smbus
+sudo adduser pi i2c
 
 echo "Done setting up I2C. Here are the devices found"
 i2cdetect -y 1
 
 echo "Installing packages for Python development with BerryIMU"
-sudo apt-get install -y python-smbus python3-smbus python-dev python3-dev
+sudo apt-get install -y  
 
 echo "Getting source code for IMU libraries and the scanner software"
 git clone https://github.com/cpre-186-group-4/Scanner.git
@@ -40,6 +45,23 @@ gcc -o gyro_accelerometer_tutorial03 -l rt gyro_accelerometer_tutorial03.c -lm
 echo "Enabling SSH"
 sudo systemctl enable ssh
 sudo systemctl start ssh
+
+echo "Enabling PiCamera"
+# Stop if /boot is not a mountpoint
+echo "Checking for boot directory"
+if ! mountpoint -q /boot; then
+echo "Uh oh, the boot folder doesn't exist. Are you sure this is a Raspberry Pi?"
+  return 1
+fi
+echo "Appending PiCamera configuration to boot.txt"
+set_config_var start_x 1 /boot/config.txt
+CUR_GPU_MEM=$(get_config_var gpu_mem /boot/config.txt)
+if [ -z "$CUR_GPU_MEM" ] || [ "$CUR_GPU_MEM" -lt 128 ]; then
+echo "Expanding GPU memory to 128 megabytes"
+  set_config_var gpu_mem 128 /boot/config.txt
+fi
+sed /boot/config.txt -i -e "s/^startx/#startx/"
+sed /boot/config.txt -i -e "s/^fixup_file/#fixup_file/"
 
 echo "SSH enabled. Connect via SSH or SFTP to the Pi's address: "
 hostname -I
